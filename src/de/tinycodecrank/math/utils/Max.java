722765a1ad562943c528a72c58b373b42763e541
tinycodecrank <38176554+tinycodecrank@users.noreply.github.com>
2021-10-12 19:51:51 +0200

    initial commit

---
+++ src/de/tinycodecrank/math/utils/Max.java
@@ -0,0 +1,39 @@
+package de.tinycodecrank.math.utils;
+
+public class Max
+{
+	public static byte max(byte a, byte b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static short max(short a, short b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static char max(char a, char b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static int max(int a, int b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static long max(long a, long b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static float max(float a, float b)
+	{
+		return a < b ? b : a;
+	}
+	
+	public static double max(double a, double b)
+	{
+		return a < b ? b : a;
+	}
+}
---
+++ src/de/tinycodecrank/math/utils/Min.java
@@ -0,0 +1,39 @@
+package de.tinycodecrank.math.utils;
+
+public class Min
+{
+	public static byte min(byte a, byte b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static short min(short a, short b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static char min(char a, char b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static int min(int a, int b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static long min(long a, long b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static float min(float a, float b)
+	{
+		return a > b ? b : a;
+	}
+	
+	public static double min(double a, double b)
+	{
+		return a > b ? b : a;
+	}
+}
